f5146cc2b159dc08f233f831f491d3de59f545fc
Petri <peewter@gmail.com>
2023-10-13 17:26:36 +0300

    Test polygon draw on the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { PolygonF, GoogleMap, useLoadScript } from '@react-google-maps/api';
 
 const Map = () => {
   const API_KEY: string = import.meta.env.VITE_APP_GOOGLE_API_KEY || '';
@@ -6,6 +6,13 @@ const Map = () => {
     googleMapsApiKey: API_KEY,
   });
   const center = { lat: 61.4981, lng: 23.7608 };
+  const coords = [
+    { lat: 61.4919503, lng: 23.7555267 },
+    { lat: 61.4919321, lng: 23.7555316 },
+    { lat: 61.4919878, lng: 23.7563182 },
+    { lat: 61.4920046, lng: 23.7563106 },
+    { lat: 61.4919502, lng: 23.7555266 },
+  ];
 
   return (
     <div className='map'>
@@ -16,7 +23,19 @@ const Map = () => {
           mapContainerClassName='map-container'
           center={center}
           zoom={15}
-        />
+        >
+          <PolygonF
+            path={coords}
+            key={1}
+            options={{
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+            }}
+          />
+        </GoogleMap>
       )}
     </div>
   );
