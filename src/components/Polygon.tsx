24a5406b3ea61fd7c874d9f96ca57cc50e15ead9
Petri <peewter@gmail.com>
2023-10-19 15:47:24 +0300

    Add opening info window when a polygon is clicked

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,14 +1,23 @@
-import { PolygonF } from '@react-google-maps/api';
+import { useState } from 'react';
+import { PolygonF, InfoWindowF } from '@react-google-maps/api';
 import transformCoordinate from '../utils/transformCoordinate';
 import data from '../data.json';
-
+type PolygonType = {
+  id: string;
+};
 const Polygon = () => {
+  const [selectedPolygon, setSelectedPolygon] = useState<PolygonType | null>(
+    null,
+  );
+
+  const [selectedPolygonPosition, setSelectedPolygonPosition] =
+    useState(undefined);
   const geoJSONFeatures = data.features;
 
   return (
     <div>
       {geoJSONFeatures.map((feature, index) => {
-        // Map the all of the coordinates from the GeoJSON data
+        // Map all of the coordinates from the GeoJSON data
         // and transform the coordinates to a Google Maps compatible form
         const coordinateArray = feature.geometry.coordinates[0][0];
         const coordinates = coordinateArray.map(
@@ -31,9 +40,24 @@ const Polygon = () => {
               fillColor: '#FF0000',
               fillOpacity: 0.35,
             }}
+            onClick={() => {
+              feature === selectedPolygon
+                ? setSelectedPolygon(null)
+                : setSelectedPolygon(feature);
+              setSelectedPolygonPosition(coordinates[0]);
+            }}
           />
         );
       })}
+      {selectedPolygon && (
+        <InfoWindowF position={selectedPolygonPosition}>
+          <div>
+            {/* Content for the InfoWindow */}
+            <h3>{selectedPolygon.id}</h3>
+            {/* Add other content here */}
+          </div>
+        </InfoWindowF>
+      )}
     </div>
   );
 };
